330f9db289692e14ef17ecce2dd6bc1de9269f94
Arthur Nersisyan <24681914+Annonymos3108@users.noreply.github.com>
2021-03-17 00:03:10 +0300

    Added Armenian-Unicode_Converter Word AddIn project

---
+++ Armenian-Uniocode-Converter/Armenian-Uniocode-Converter/Band.Designer.cs
@@ -0,0 +1,130 @@
+﻿
+namespace Armenian_Uniocode_Converter
+{
+    partial class Band : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Band()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором компонентов
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Unicode_Converter = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.btn_Armenian_Covert_to_Unicode = this.Factory.CreateRibbonButton();
+            this.btn_Convert_to_unicode_and_save_to_new_file = this.Factory.CreateRibbonButton();
+            this.Save_to_pdf = this.Factory.CreateRibbonGroup();
+            this.btn_Save_to_PDF = this.Factory.CreateRibbonButton();
+            this.Unicode_Converter.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.Save_to_pdf.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Unicode_Converter
+            // 
+            this.Unicode_Converter.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.Unicode_Converter.Groups.Add(this.group1);
+            this.Unicode_Converter.Groups.Add(this.Save_to_pdf);
+            this.Unicode_Converter.Label = "Armenian Unicode Converter";
+            this.Unicode_Converter.Name = "Unicode_Converter";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.btn_Armenian_Covert_to_Unicode);
+            this.group1.Items.Add(this.btn_Convert_to_unicode_and_save_to_new_file);
+            this.group1.Label = "Armenian";
+            this.group1.Name = "group1";
+            // 
+            // btn_Armenian_Covert_to_Unicode
+            // 
+            this.btn_Armenian_Covert_to_Unicode.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_Armenian_Covert_to_Unicode.Label = "Convert to Unicode";
+            this.btn_Armenian_Covert_to_Unicode.Name = "btn_Armenian_Covert_to_Unicode";
+            this.btn_Armenian_Covert_to_Unicode.OfficeImageId = "FontsReplaceFonts";
+            this.btn_Armenian_Covert_to_Unicode.ShowImage = true;
+            this.btn_Armenian_Covert_to_Unicode.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_Armenian_Covert_to_Unicode_Click);
+            // 
+            // btn_Convert_to_unicode_and_save_to_new_file
+            // 
+            this.btn_Convert_to_unicode_and_save_to_new_file.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_Convert_to_unicode_and_save_to_new_file.Label = "Convert to unicode and save to а new file";
+            this.btn_Convert_to_unicode_and_save_to_new_file.Name = "btn_Convert_to_unicode_and_save_to_new_file";
+            this.btn_Convert_to_unicode_and_save_to_new_file.OfficeImageId = "FontsReplaceFonts";
+            this.btn_Convert_to_unicode_and_save_to_new_file.ShowImage = true;
+            this.btn_Convert_to_unicode_and_save_to_new_file.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_Convert_to_unicode_and_save_to_new_file_Click);
+            // 
+            // Save_to_pdf
+            // 
+            this.Save_to_pdf.Items.Add(this.btn_Save_to_PDF);
+            this.Save_to_pdf.Label = "Save to PDF";
+            this.Save_to_pdf.Name = "Save_to_pdf";
+            // 
+            // btn_Save_to_PDF
+            // 
+            this.btn_Save_to_PDF.ControlSize = Microsoft.Office.Core.RibbonControlSize.RibbonControlSizeLarge;
+            this.btn_Save_to_PDF.Label = "Save to PDF";
+            this.btn_Save_to_PDF.Name = "btn_Save_to_PDF";
+            this.btn_Save_to_PDF.OfficeImageId = "FileSaveAsPdfOrXps";
+            this.btn_Save_to_PDF.ShowImage = true;
+            this.btn_Save_to_PDF.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.btn_Save_to_PDF_Click);
+            // 
+            // Band
+            // 
+            this.Name = "Band";
+            this.RibbonType = "Microsoft.Word.Document";
+            this.Tabs.Add(this.Unicode_Converter);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Band_Load);
+            this.Unicode_Converter.ResumeLayout(false);
+            this.Unicode_Converter.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.Save_to_pdf.ResumeLayout(false);
+            this.Save_to_pdf.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab Unicode_Converter;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_Armenian_Covert_to_Unicode;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_Convert_to_unicode_and_save_to_new_file;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup Save_to_pdf;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton btn_Save_to_PDF;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Band Band
+        {
+            get { return this.GetRibbon<Band>(); }
+        }
+    }
+}
---
+++ Armenian-Uniocode-Converter/Armenian-Uniocode-Converter/Band.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Office.Tools.Ribbon;
+using Word = Microsoft.Office.Interop.Word;
+using System.Windows.Forms;
+
+namespace Armenian_Uniocode_Converter
+{
+    public partial class Band
+    {
+        private void Band_Load(object sender, RibbonUIEventArgs e)
+        {
+        }
+
+        private void FindAndReplace(Microsoft.Office.Interop.Word.Application wordApp,
+            object toFindText, object replaceWithText)
+        {
+            object matchCase = true;
+            object matchwholeWord = false;
+            object matchwildCards = false;
+            object matchSoundLike = false;
+            object nmatchAllforms = false;
+            object forward = true;
+            object format = false;
+            object matchKashida = false;
+            object matchDiactitics = false;
+            object matchAlefHamza = false;
+            object matchControl = false;
+            object read_only = false;
+            object visible = true;
+            object replace = 2;
+            object wrap = 1;
+            
+            wordApp.Selection.Find.Execute(ref toFindText, ref matchCase,
+                                            ref matchwholeWord, ref matchwildCards,
+                                            ref matchSoundLike, ref nmatchAllforms,
+                                            ref forward, ref wrap, ref format,
+                                            ref replaceWithText, ref replace,
+                                            ref matchKashida, ref matchDiactitics,
+                                            ref matchAlefHamza, ref matchControl);
+        }
+
+        private void Convert_to_Unicode(Microsoft.Office.Interop.Word.Application wordApp)
+        {
+            //markers
+            //question mark // ?            
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)177),
+                                    char.ConvertFromUtf32((int)1374));
+
+            //emphasis // '
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)176),
+                                    char.ConvertFromUtf32((int)1371));
+
+            //exclamation mark // !
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)175),
+                                    char.ConvertFromUtf32((int)1372));
+
+            //punctuation mark //՝
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)170),
+                                    char.ConvertFromUtf32((int)1373));
+
+            // ' //apatarts
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)254),
+                                    char.ConvertFromUtf32((int)1370));
+
+            // letter "Yev"
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)168),
+                                    char.ConvertFromUtf32((int)1415));
+
+            //small letters
+            int i = 1377;
+            int j = 1198;
+            int x = 0;
+            do
+            {
+                x = i - j;
+                FindAndReplace(wordApp, char.ConvertFromUtf32((int)x),
+                                        char.ConvertFromUtf32((int)i));
+                i++;
+                j--;
+            }
+            while (i < 1415);
+
+            //capital letters
+            int k = 1329;
+            int l = 1151;
+            int c = 0;
+            do
+            {
+                c = k - l;
+                FindAndReplace(wordApp, char.ConvertFromUtf32((int)c),
+                                        char.ConvertFromUtf32((int)k));
+                k++;
+                l--;
+            } while (k < 1367);
+
+            //Quotes // " " //<<  >>
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)166), 
+                                    char.ConvertFromUtf32((int)'»'));
+            FindAndReplace(wordApp, char.ConvertFromUtf32((int)167), 
+                                    char.ConvertFromUtf32((int)'«'));
+        }
+
+        private void btn_Armenian_Covert_to_Unicode_Click(object sender, 
+            RibbonControlEventArgs e)
+        {
+            Convert_to_Unicode(Globals.ThisAddIn.Application);
+            Globals.ThisAddIn.Application.ActiveDocument.Save();
+            MessageBox.Show("Document converted successfully.\nBest Regards, Arthur Nersisyan",
+                "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_Convert_to_unicode_and_save_to_new_file_Click(object sender,
+            RibbonControlEventArgs e)
+        {
+            Word.Document refDoc = Globals.ThisAddIn.Application.ActiveDocument;
+            refDoc.Save();
+            string refName = refDoc.Name.Substring(0, refDoc.Name.Length - 5);
+            string newDocFullName = refDoc.Path + "\\" + refName + "_unicode.docx";
+            refDoc.SaveAs2(newDocFullName);              
+            Word.Application newWordApp = Globals.ThisAddIn.Application;
+            Convert_to_Unicode(newWordApp);
+            newWordApp.ActiveDocument.Save();
+            MessageBox.Show("Document converted and saved to a new file.\n" +
+               "The file is located in the same directory as the original one.",
+               "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btn_Save_to_PDF_Click(object sender, RibbonControlEventArgs e)
+        {
+            Word.Document doc = Globals.ThisAddIn.Application.ActiveDocument;
+            string sfileName_Document = doc.Name.Substring(0, doc.Name.Length - 5);
+            string sPath = doc.Path;
+            string sFullpath_pdf = sPath + "\\" + sfileName_Document + ".pdf";
+            doc.ExportAsFixedFormat(sFullpath_pdf, Word.WdExportFormat.wdExportFormatPDF,
+                OpenAfterExport: true);
+        }
+    }
+}
